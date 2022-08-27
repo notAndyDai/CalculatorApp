f1636d7ea89c64bb297b16f75c3bd04f5e2c4526
AndyDai602 <andydai602@gmail.com>
2022-08-26 20:04:47 -0400

    pushing code

---
+++ script.js
@@ -0,0 +1,15 @@
+function clearScreen(){
+    let displayBox = document.getElementById("result");
+    displayBox.value = "";
+}
+
+function display(value){
+    let displayBox = document.getElementById("result");
+    displayBox.value += value;
+}
+
+function calculate(){
+    let displayBox = document.getElementById("result").value;
+    let answer = eval(displayBox);
+    document.getElementById("result").value = answer;
+}
